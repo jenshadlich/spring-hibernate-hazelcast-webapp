a221be0e7441ebfaabf19d0567ea6797096bd66b
Jens Hadlich <jenshadlich@googlemail.com>
2011-07-19 12:33:57 +0200

    update

--- struts2/src/test/java/de/jeha/spring_hibernate_hazelcast_webapp/struts2/HazelcastClusterTest.java
+++ struts2/src/test/java/de/jeha/spring_hibernate_hazelcast_webapp/struts2/HazelcastClusterTest.java
@@ -1,13 +1,19 @@
 package de.jeha.spring_hibernate_hazelcast_webapp.struts2;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.perf4j.LoggingStopWatch;
+import org.perf4j.StopWatch;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hazelcast.core.Hazelcast;
 
 import de.jeha.spring_hibernate_hazelcast_webapp.struts2.bl.GenericDao;
 import de.jeha.spring_hibernate_hazelcast_webapp.struts2.entities.User;
@@ -21,7 +27,15 @@ public class HazelcastClusterTest {
 
 	@Test
 	public void foobar() {
+		StopWatch stopWatch = new LoggingStopWatch();
 
+		Map<String, String> map = Hazelcast.getMap("values");
+
+		for (int i = 0; i < 1 * 1000; i++) {
+			map.put(Integer.toString(i), "value" + i);
+		}
+
+		stopWatch.stop("done");
 	}
 
 	@Ignore
